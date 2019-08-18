e37c93e71d6b23f66fc6f3f3383f3b2a40f6eac4
Jorge Luis <jorge.luis4212@gmail.com>
2019-08-18 14:38:52 -0400

    finished prework

---
+++ remove-anchors.js
@@ -0,0 +1,9 @@
+// create a system to read the string
+// once the string is read it has to identify #
+//after identifying #, the characters afterwards should be removed
+
+
+function removeUrlAnchor(url) {
+    return url.split('#')[0]
+   
+   }
